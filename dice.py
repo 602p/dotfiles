5d099940fb7f3ea88314d65b43bfb01cea7e5c3a
Louis Goessling [lld5hp] <louis@goessling.com>
2017-03-19 11:56:28 -0500

    Add a bunch of tool stuff

---
+++ dice.py
@@ -0,0 +1,29 @@
+import random, sys, colorama
+from colorama import Fore, Back, Style
+
+colorama.init()
+
+if len(sys.argv)!=2:
+	print(Fore.RED+"E: Use as `dice 1d4+7`")
+	sys.exit()
+
+value=0
+for thing in sys.argv[1].split("+"):
+	thing=thing.strip()
+	if thing:
+		if "d" in thing:
+			if thing.split("d")[0]=="" or int(thing.split("d")[0])==1:
+				roll=random.randint(1, int(thing.split("d")[1]))
+				if int(thing.split("d")[1])==20:
+					if roll==20:
+						print(" ", Back.GREEN, " ", Style.RESET_ALL, Fore.GREEN, " Nat 20 on roll!", Style.RESET_ALL, sep="")
+					elif roll==1:
+						print(" ", Back.RED, " ", Style.RESET_ALL, Fore.RED, " Nat 1 on roll!", Style.RESET_ALL, sep="")
+				value+=roll
+			else:
+				rolls=[random.randint(1, int(thing.split("d")[1])) for _ in range(int(thing.split("d")[0]))]
+				value+=sum(values)
+		else:
+			value+=int(thing)
+
+print("=> ", Fore.CYAN, value, Style.RESET_ALL, sep="")
--- wifimanager.py
+++ wifimanager.py
@@ -11,54 +11,70 @@ def runcmd(cmdline):
 	return subprocess.Popen(cmdline, shell=True,stdout=subprocess.PIPE).stdout
 
 def get_wifi_options():
+	#return []
 	#return [WiFiNetwork("ABCD", []), WiFiNetwork("DEEZNUTS", ["WPA"]), WiFiNetwork("cucktown", ['a', 'b', 'c'])]
 	lines=[line.decode("ascii", errors="ignore") for line in runcmd("nmcli d wifi list").readlines()]
 	wifis=[]
 	for line in lines[1:]:
 		name=line[3:].split(" ")[0]
 		security=[]
-		for sectype in ["WPA1", "WPA2", "TKIP", "WEP"]:
+		for sectype in ["WPA1", "WPA2", "TKIP", "WEP", "802.1X"]:
 			if line.find(sectype)!=-1:
 				security.append(sectype)
-		if name!="--":
-			wifis.append(WiFiNetwork(name=name, security=security))
+		wifis.append(WiFiNetwork(name=name if name!="--" else "<Hidden>", security=security))
 	return wifis
 
 def main(scr):
 	curses.noecho()
 	curses.cbreak()
+	scr.nodelay(1)
 	networks=get_wifi_options()
 	selected=0
 	last_refresh=datetime.datetime.now()
+	last_poll_time=last_refresh.second
+	ticker="|/-\\"
+	ticker_pos=0
 	while 1:
 		scr.clear()
 
 		scr.addstr(0, 0, "(q) To Quit, (r) To Refresh, (Up) and (Down) to navigate, and (Enter) to connect")
-		scr.addstr(1, 0, "WiFi Networks Availible: (Last Refresh ")
+		scr.addstr(1, 0, "WiFi Networks Availible: (Scanning... %s, Last Refresh "%ticker[ticker_pos])
 		scr.addstr(str(last_refresh))
 		scr.addstr(")")
 		scr.addstr(2, 0, "="*100)
 
+		i=0
+
 		if len(networks)==0:
 			scr.addstr(3, 0, "No networks availible")
 		else:
-			for i, network in enumerate(networks):
+			for network in networks:
 				scr.addstr(3+i, 0, " x " if i==selected else " o ")
 				scr.addstr(network.name+": "+(", ".join(network.security) if network.security else "Unsecured"), curses.A_REVERSE if i==selected else 0)
-		
-		key=scr.getkey()
+				i+=1
+
+		scr.addstr(4+i, 0, " x " if i==selected else " o ")
+		scr.addstr("Manual/Hidden Network (Enter SSID)", curses.A_REVERSE if i==selected else 0)
+		try:
+			key=scr.getkey()
+		except:
+			key=""
 		if key.lower()=="q":
 			break
-		elif key.lower()=="r":
+		if key.lower()=="r" or (datetime.datetime.now()-last_refresh).seconds>0:
 			networks=get_wifi_options()
 			last_refresh=datetime.datetime.now()
-			if selected>=len(networks):
+			ticker_pos+=1
+			if ticker_pos==len(ticker):
+				ticker_pos=0
+			if selected>len(networks):
 				selected=len(networks)-1
-		elif key=="KEY_UP":
+		if key=="KEY_UP":
 			selected=selected-1 if selected>0 else 0
-		elif key=="KEY_DOWN":
-			selected=selected+1 if selected<len(networks)-1 else len(networks)-1
-		elif key=="\n":
+		if key=="KEY_DOWN":
+			selected=selected+1 if selected<len(networks) else len(networks)
+		if key=="\n":
+			scr.nodelay(0)
 			if selected<len(networks) and selected>-1:
 				command="nmcli dev wifi con \"%s\""%networks[selected].name
 				if networks[selected].security:
@@ -69,11 +85,20 @@ def main(scr):
 					curses.echo()
 					password=scr.getstr()
 					command+=" password \"%s\""%password.decode("ascii")
-				curses.endwin()
-				os.system("clear")
-				print("Running `%s`..."%command)
-				os.system(command)
-				return
+			elif selected==len(networks):
+				scr.clear()
+				scr.addstr(0, 0, "Connecting to manual WiFi (Security type(s) ?)...")
+				scr.addstr(1, 0, "Enter SSID:")
+				curses.echo()
+				network=scr.getstr().decode("ascii")
+				scr.addstr(2, 0, "Enter Password:")
+				password=scr.getstr().decode("ascii")
+				command="nmcli dev wifi con \"%s\" password \"%s\""%(network, password)
+			curses.endwin()
+			os.system("clear")
+			print("Running `%s`..."%command)
+			os.system(command)
+			return
 		scr.refresh()
 
 try:
