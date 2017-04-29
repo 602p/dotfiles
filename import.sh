21e7f8b5104055232cd933486573e5dd58aa29b9
Louis Goessling [lld5hp] <louis@goessling.com>
2017-04-28 19:39:10 -0500

    Long overdue import

--- import.sh
+++ import.sh
@@ -1,4 +1,8 @@
 rm -rf files/*
 mkdir files/i3
+mkdir files/sublime-text-3
 cp -r ~/.i3/* files/i3
 cp ~/.bash_aliases files/bash_aliases
+cp ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings files/sublime-text-3
+cp "$(echo ~)/.config/sublime-text-3/Packages/User/Default (Linux).sublime-keymap" files/sublime-text-3
+cp ~/.config/sublime-text-3/Packages/Boxy\ Theme/schemes/* files/sublime-text-3
