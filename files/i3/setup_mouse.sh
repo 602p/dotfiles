4cfe51afd86892d92b62012447ac08ed1617b9c2
Louis Goessling <louis@goessling.com>
2017-06-05 22:05:20 -0500

    Really fix mouse

---
+++ files/i3/setup_mouse.sh
@@ -0,0 +1,2 @@
+xinput --set-prop $(xinput list --short | grep "PS/2 Generic Mouse" | python3 -c "i=input();print(i[i.index('=')+1:i.index('[')])") "Evdev Middle Button Emulation" 1
+xinput --set-prop $(xinput list --short | grep "PS/2 Generic Mouse" | python3 -c "i=input();print(i[i.index('=')+1:i.index('[')])") "Device Accel Constant Deceleration" 0.45
