ca821470c5ed93415718498b140a19d13c971f20
Louis Goessling <louis@goessling.com>
2017-06-05 22:07:16 -0500

    less bad mouse script

--- files/i3/setup_mouse.sh
+++ files/i3/setup_mouse.sh
@@ -1,2 +1,4 @@
-xinput --set-prop $(xinput list --short | grep "PS/2 Generic Mouse" | python3 -c "i=input();print(i[i.index('=')+1:i.index('[')])") "Evdev Middle Button Emulation" 1
-xinput --set-prop $(xinput list --short | grep "PS/2 Generic Mouse" | python3 -c "i=input();print(i[i.index('=')+1:i.index('[')])") "Device Accel Constant Deceleration" 0.45
+MOUSEID="$(xinput list --short | grep "PS/2 Generic Mouse" | python3 -c "i=input();print(i[i.index('=')+1:i.index('[')])")"
+
+xinput --set-prop $MOUSEID "Evdev Middle Button Emulation" 1
+xinput --set-prop $MOUSEID "Device Accel Constant Deceleration" 0.5
