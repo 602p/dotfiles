4a0718f3834661e5b94837b2be9d4e5cdb3eb387
Louis Goessling <louis@goessling.com>
2018-02-01 13:36:22 -0600

    Remove more crud, add circled numbers

--- files/i3/make_set_fullwidth.py
+++ files/i3/make_set_fullwidth.py
@@ -2,8 +2,8 @@ hexn = lambda c:hex(ord(c))[2:]
 
 with open("set_fullwidth.sh", 'w') as fd:
 	fd.write("xmodmap -pke > ~/.i3/old.xmodmap\n")
-	alpha="abcdefghijklmnopqrstuvwxyz"
+	alpha="abcdefghijklmnopqrstuvwxyz1234567890"
 	for char in alpha:
-		lower=("ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ"[alpha.index(char)])
-		upper=("ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ"[alpha.index(char.lower())])
+		lower=("ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ①②③④⑤⑥⑦⑧⑨⓪"[alpha.index(char)])
+		upper=("ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ①②③④⑤⑥⑦⑧⑨⓪"[alpha.index(char.lower())])
 		fd.write("xmodmap -e \"keysym "+char+" = U"+hexn(lower)+" U"+hexn(upper)+"\"\n")
