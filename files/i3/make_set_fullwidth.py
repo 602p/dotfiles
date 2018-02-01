f21887d10a59c36317507a814222f45b67c1865b
Louis Goessling <louis@goessling.com>
2018-02-01 13:40:15 -0600

    Keep shifted numbers mapped normally

--- files/i3/make_set_fullwidth.py
+++ files/i3/make_set_fullwidth.py
@@ -5,5 +5,5 @@ with open("set_fullwidth.sh", 'w') as fd:
 	alpha="abcdefghijklmnopqrstuvwxyz1234567890"
 	for char in alpha:
 		lower=("ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ①②③④⑤⑥⑦⑧⑨⓪"[alpha.index(char)])
-		upper=("ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ①②③④⑤⑥⑦⑧⑨⓪"[alpha.index(char.lower())])
+		upper=("ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ!@#$%^&*()"[alpha.index(char.lower())])
 		fd.write("xmodmap -e \"keysym "+char+" = U"+hexn(lower)+" U"+hexn(upper)+"\"\n")
