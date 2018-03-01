afef7cc2f6f97fe984886384350839053c9debb1
Louis Goessling <louis@goessling.com>
2018-02-28 19:56:13 -0600

    Switch back to wicd-gtk and include ssh config

--- import.sh
+++ import.sh
@@ -1,6 +1,7 @@
 rm -rf files/*
 mkdir files/i3
 mkdir files/sublime-text-3
+mkdir files/ssh
 cp -r ~/.i3/* files/i3
 cp ~/.bash_aliases files/bash_aliases
 cp ~/.bashrc files/bashrc
@@ -10,3 +11,4 @@ cp ~/.config/sublime-text-3/ files/sublime-text-3 -r
 #cp ~/.config/sublime-text-3/Packages/Boxy\ Theme/schemes/* files/sublime-text-3
 cp ~/.Xresources files/Xresources
 rm -rf files/sublime-text-3/sublime-text-3/Index
+cp ~/.ssh/config files/ssh/config
