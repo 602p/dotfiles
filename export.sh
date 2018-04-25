58b9a9caf8fca588a5561e06a2edf23776a0dd05
Louis Goessling <louis@goessling.com>
2018-04-25 18:47:15 -0500

    boxrc

--- export.sh
+++ export.sh
@@ -2,3 +2,9 @@ cp files/bashrc ~/.bashrc
 cp files/bash_aliases ~/.bash_aliases
 mkdir ~/.ssh
 cp -r files/ssh/* ~/.ssh
+if [ ! -e ~/.boxrc ]
+then
+	cp files/min_boxrc ~/.boxrc
+else
+	echo ".boxrc present"
+fi
