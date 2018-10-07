ff0aec404d7a0d2e3d82a347e176c15fafd9a110
Louis Goessling <louis@goessling.com>
2018-10-07 11:52:55 -0500

    leansho

---
+++ files/i3/do_leanshot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+SCREENSHOT=$HOME/.i3/leanshot
+# choose some file to save it to
+FILE="$HOME/last_screenshot.png"
+$SCREENSHOT selection -o $FILE
+# optional: copy to clipboard
+XCLIP=/usr/bin/xclip
+$XCLIP -selection clipboard -t image/png -i $FILE
